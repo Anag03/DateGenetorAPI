5a81669aaa76e95c55f6cee7e2cfb83ead3c8c3e
Anag03 <anagallardo03@gmail.com>
2023-05-04 00:00:25 +0100

    Updating files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const home = require("./routes/home");
 const app = express();
 app.use(express.json());
+
 const moment = require('moment-timezone');
 
 app.get('/random_date', (req, res) => {
@@ -20,6 +20,3 @@ app.get('/random_date', (req, res) => {
   });
 });
 
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
