efc39f2fd6ac326b82e5a8eec1830b959bb3e328
Anag03 <anagallardo03@gmail.com>
2023-05-04 02:17:38 +0100

    Updating files

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
-
 const moment = require('moment-timezone');
 
+// Parse JSON requests
+app.use(express.json());
+
+// Generate a random date within the specified range
 app.get('/random_date', (req, res) => {
-  const start_date = moment().tz('Europe/Dublin');
-  const end_date = moment().tz('Europe/Dublin');
-  const start_offset = parseInt(req.query.start) || 0;
-  const end_offset = parseInt(req.query.end) || 0;
-  const random_date = new Date(start_date.valueOf() + start_offset + Math.random() * (end_date.valueOf() - start_date.valueOf() + end_offset));
-  const start_date_modified = new Date(start_date.valueOf() + start_offset);
-  const end_date_modified = new Date(end_date.valueOf() + end_offset);
-  const start_datetime = start_date_modified.toISOString().substring(0, 10) + ' ' + start_date_modified.toLocaleTimeString('en-GB');
-  const end_datetime = end_date_modified.toISOString().substring(0, 10) + ' ' + end_date_modified.toLocaleTimeString('en-GB');
+  // Get current date/time in the 'Europe/Dublin' timezone
+  const currentDate = moment().tz('Europe/Dublin');
+
+  // Parse start and end date offsets from request query parameters
+  const startOffset = parseInt(req.query.start) || 0;
+  const endOffset = parseInt(req.query.end) || 0;
+
+  // Calculate start and end date/time based on current date/time and offsets
+  const startDate = new Date(currentDate.valueOf() + startOffset);
+  const endDate = new Date(currentDate.valueOf() + endOffset);
+
+  // Generate a random date/time between start and end dates
+  const randomDate = new Date(startDate.valueOf() + Math.random() * (endDate.valueOf() - startDate.valueOf()));
+
+  // Format start and end date/time as ISO strings with timezones
+  const startDateTime = startDate.toISOString().substring(0, 10) + ' ' + startDate.toLocaleTimeString('en-GB');
+  const endDateTime = endDate.toISOString().substring(0, 10) + ' ' + endDate.toLocaleTimeString('en-GB');
+
+  // Send the start and end date/time as a JSON response
   res.send({
-    start_datetime: start_datetime,
-    end_datetime: end_datetime,
+    startDateTime: startDateTime,
+    endDateTime: endDateTime,
   });
 });
 
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
